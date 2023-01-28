559b15d90ab3b282ab068da75f021f3692bf1a96
Simon Eric Noumedem <kkenzo2007@yahoo.fr>
2023-01-28 19:09:22 +0100

    Add task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path, notice: "Task was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "Task was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: "Task was successfully destroyed."
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name)
+  end
+  def set_task
+    Task.find(params[:id])
+  end
+end

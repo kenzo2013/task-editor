6a48f3dedc25e3fec0ef60348befde6f2c646db7
Simon Eric Noumedem <kkenzo2007@yahoo.fr>
2023-02-01 23:17:48 +0100

    Edit and delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,7 +29,10 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to tasks_path, notice: "Task was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to tasks_path, notice: "Task was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Task was successfully updated." }
+      end
     else
       render :edit
     end
@@ -37,7 +40,10 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_path, notice: "Task was successfully destroyed."
+    respond_to do |format|
+      format.html { redirect_to tasks_path, notice: "Task was successfully destroyed." }
+      format.turbo_stream { flash.now[:notice] = "Task was successfully destroyed." }
+    end
   end
 
   private
